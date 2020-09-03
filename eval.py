d509a4e36f83f5b6a46c828dfaf8720e1134be7f
Elle O'Brien <elleobrien11@gmail.com>
2020-09-02 21:50:43 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -7,10 +7,10 @@ model = pickle.load(open("models/model.pkl", "rb"))
 
 # Load the test data
 X_test = pd.read_csv("data/test_features.csv")
-y_test = pd.read_csv("data/test_y.csv")
+y_test = pd.read_csv("data/test_y.csv").values
 
 # Test on the model
-test_r2 = model.score(X_test, y_test)
+test_r2 = model.score(X_test, y_test.ravel())
 
 with open('test_metrics.json', 'w') as f:
     json.dump({'r2': test_r2}, f)
